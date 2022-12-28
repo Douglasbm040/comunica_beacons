331d2b0494225639e76792cc5759d28804784555
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2022-12-27 23:11:43 -0300

    refactoring - separando view-controller-model

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-// ignore_for_file: unused_import
-
-import 'package:comunica_beacons/src/core/bluetooth/ble_interation.dart';
-import 'package:comunica_beacons/src/core/bluetooth/scanner.dart';
+import 'package:comunica_beacons/src/controllers/controller_interationble.dart';
+import 'package:comunica_beacons/src/controllers/controller_conectble.dart';
+import 'package:comunica_beacons/src/controllers/controller_scannerble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:provider/provider.dart';
-import 'src/core/bluetooth/ble_conect.dart';
-import 'src/core/bluetooth/ble_scanner.dart';
-import 'app.dart';
+import 'src/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,11 +12,12 @@ void main() {
   final FlutterReactiveBle _ble = FlutterReactiveBle();
 
   runApp(MultiProvider(providers: [
-    ChangeNotifierProvider<Scan>(create: (context) => Scan(ble: _ble)),
-    ChangeNotifierProvider<BleConnector>(
-        create: ((context) => BleConnector(ble: _ble))),
+    ChangeNotifierProvider<ControllerScannerBle>(
+        create: (context) => ControllerScannerBle(ble: _ble)),
+    ChangeNotifierProvider<ControllerConnectorBle>(
+        create: ((context) => ControllerConnectorBle(ble: _ble))),
     ChangeNotifierProvider(
-        create: (context) => BleIntegration(
+        create: (context) => ControllerIntegrationBle(
             bleDiscoverServices: _ble.discoverServices,
             readcharacteristic: _ble.readCharacteristic))
   ], child: const App()));
--- lib/src/screens/comunication_page.dart
+++ lib/src/screens/comunication_page.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:comunica_beacons/src/core/bluetooth/ble_interation.dart';
-import '../ui/comunication_ui.dart';
+import 'package:comunica_beacons/src/controllers/controller_interationble.dart';
+import '../widget/comunication_ui.dart';
 
 class ComunicationPage extends StatelessWidget {
   const ComunicationPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Consumer<BleIntegration>(
-        builder: (context, integration, child) => ComunicationUI(
-              comunica: integration,
+    return Consumer<ControllerIntegrationBle>(
+        builder: (context, plugs, child) => ComunicationUI(
+              comunication: plugs,
             ));
   }
 }
